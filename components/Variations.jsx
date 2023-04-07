5c171ee9c06f19b9f121d2993c86ad39ec56e148
Cristian Wargny <bh.cristian22@gmail.com>
2023-04-06 20:11:45 -0400

    render unavailable variations as disabled buttons

--- components/Variations.jsx
+++ components/Variations.jsx
@@ -8,6 +8,15 @@ const Variations = ({variations}) => {
 
     const handleOnClick = (id, value) => {
         if (id in selectedVari){
+            if (selectedVari[id] === value){
+                setSelectedVari(prevState => {
+                    const newSelected = {...prevState}
+                    delete newSelected[id]
+                    return newSelected
+                })
+                return
+            }
+            
             setSelectedVari(prevState => {
                 const newSelected = {...prevState}
                 newSelected[id] = value
@@ -18,13 +27,31 @@ const Variations = ({variations}) => {
         }
     }
 
+    const isButtonDisabled = (id, value) => {
+        if (!Object.keys(selectedVari).length) return false
+
+        const isVariAvailable = (comb) => {
+            for (const elem in selectedVari) {
+                return comb.name === elem && comb.value_name === selectedVari[elem]
+            }
+        }
+
+        const variExists = variations.some((el) => (
+            el.attribute_combinations.some((comb) => comb.name === id && comb.value_name === value) &&
+            el.attribute_combinations.some((comb) => isVariAvailable(comb))
+            
+        )) 
+        if (!variExists) return true
+
+    }
+
     
     useEffect(() => {
         const uniqueVarsIds = () => {
             const varsIds = []
             for (const vari of variations) {
                 for (const atr of vari.attribute_combinations) {
-                    varsIds.push(atr.id)
+                    varsIds.push(atr.name)
                 }
             }
             return [...new Set(varsIds)]
@@ -37,7 +64,7 @@ const Variations = ({variations}) => {
 
             for (const vari of variations) {
                 for (const val of vari.attribute_combinations) {
-                    varsObj[val.id] = [...new Set([...varsObj[val.id], val.value_name])]
+                    varsObj[val.name] = [...new Set([...varsObj[val.name], val.value_name])]
                 }
             }
             return varsObj
@@ -59,7 +86,7 @@ const Variations = ({variations}) => {
                         <div key={el}>
                             <p>{el}</p>
                                 {uniqueVaris[el].map((vari) => (
-                                    <button onClick={() => handleOnClick(el, vari)} key={vari} disabled={false}>{vari}</button>
+                                    <button onClick={() => handleOnClick(el, vari)} key={vari} disabled={isButtonDisabled(el, vari)}>{vari}</button>
                                 ))}
                         </div>
                     ))}
